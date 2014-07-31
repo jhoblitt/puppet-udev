afd6ec5e1b221f49c7382aac836541109e71ba3c
Paulo E. Castro <pauloedgarcastro@gmail.com>
2014-07-31 18:51:42 +0100

    Reinstate the operatingsystem check.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,39 +5,40 @@
 class udev::params {
   $udevadm_path = '/sbin'
 
-  case $::operatingsystem {
+  case $::osfamily {
     'debian': {
       $udev_package = 'udev'
       $udevlogpriority = 'udevadm control --log-priority'
       $udevtrigger     = 'udevadm trigger'
     }
     'redhat': {
-      case $::operatingsystemmajrelease {
-        '5': {
-          $udev_package = 'udev'
-          $udevtrigger     = 'udevtrigger'
-          $udevlogpriority = 'udevcontrol log_priority'
-        }
-        '6','7': {
-          $udev_package = 'udev'
+      if $::operatingsystem == 'Fedora' {
+        if ($::operatingsystemmajrelease >= 20) {
+          $udev_package    = 'systemd'
           $udevtrigger     = 'udevadm trigger'
           $udevlogpriority = 'udevadm control --log-priority'
         }
-        default: {
-          fail("Module ${module_name} is not supported on RedHat release ${::operatingsystemmajrelease}")
+        else {
+          fail("Module ${module_name} might not be supported on Fedora release ${::operatingsystemmajrelease}")
+        }
+      } else {
+        case $::operatingsystemmajrelease {
+          '5': {
+            $udev_package = 'udev'
+            $udevtrigger     = 'udevtrigger'
+            $udevlogpriority = 'udevcontrol log_priority'
+          }
+          '6','7': {
+            $udev_package = 'udev'
+            $udevtrigger     = 'udevadm trigger'
+            $udevlogpriority = 'udevadm control --log-priority'
+          }
+          default: {
+            fail("Module ${module_name} is not supported on RedHat release ${::operatingsystemmajrelease}")
+          }
         }
       }
     }
-    'fedora': {
-      if ($::operatingsystemmajrelease >= 20) {
-        $udev_package    = 'systemd'
-        $udevtrigger     = 'udevadm trigger'
-        $udevlogpriority = 'udevadm control --log-priority'
-      }
-      else {
-        fail("Module ${module_name} might not be supported on Fedora release ${::operatingsystemmajrelease}")
-        }
-      }
     default: {
       fail("Module ${module_name} is not supported on ${::operatingsystem}")
     }
