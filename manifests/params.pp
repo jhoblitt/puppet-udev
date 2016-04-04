675434c574a1ab98faeb463a59e6fc4991571a02
Craig Smith <craig.smith@xtxmarkets.com>
2016-04-04 16:11:13 +0100

    use versioncmp to compare fedora release (in puppet v4 $::operatingsystemmajrelease is treated strictly as a string)

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,7 +13,7 @@ class udev::params {
     }
     'redhat': {
       if $::operatingsystem == 'Fedora' {
-        if ($::operatingsystemmajrelease >= 20) {
+        if (versioncmp($::operatingsystemmajrelease,20) >=0) {
           $udev_package    = 'systemd'
           $udevtrigger     = 'udevadm trigger'
           $udevlogpriority = 'udevadm control --log-priority'
