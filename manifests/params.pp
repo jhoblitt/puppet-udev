dcfa999075857ae6e3cbf44999980e5313ee1fc2
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2016-08-03 13:28:27 +0200

    Fixes #20
    * Explicit cast of `operatingsystemmajrelease` to String type
    * Required for Puppet v4 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,7 +22,7 @@ class udev::params {
           fail("Module ${module_name} might not be supported on Fedora release ${::operatingsystemmajrelease}")
         }
       } else {
-        case $::operatingsystemmajrelease {
+        case "${::operatingsystemmajrelease}" {
           '5': {
             $udev_package    = 'udev'
             $udevtrigger     = 'udevtrigger'
