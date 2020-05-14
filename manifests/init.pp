20f21b884ef86e85b6ac6c6f03113b19685b2428
Hendrik Frenzel <Hendrik.Frenzel@webfleet.com>
2020-05-14 11:22:06 +0200

    Added support for EL8

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,11 +27,11 @@ class udev(
   validate_re($udev_log, '^err$|^info$|^debug$')
   validate_bool($config_file_replace)
 
-  anchor { 'udev:begin': } ->
-  package{ $udev::params::udev_package:
+  anchor { 'udev:begin': }
+  -> package{ $udev::params::udev_package:
     ensure => present,
-  } ->
-  file { '/etc/udev/udev.conf':
+  }
+  -> file { '/etc/udev/udev.conf':
     ensure  => file,
     content => template("${module_name}/udev.conf.erb"),
     owner   => 'root',
@@ -39,16 +39,16 @@ class udev(
     mode    => '0644',
     replace => $config_file_replace,
     notify  => Class['udev::udevadm::logpriority'],
-  } ->
-  anchor { 'udev:end': }
+  }
+  -> anchor { 'udev:end': }
 
-  Anchor['udev:begin'] ->
-  class { 'udev::udevadm::trigger': } ->
-  Anchor['udev:end']
+  Anchor['udev:begin']
+  -> class { 'udev::udevadm::trigger': }
+  -> Anchor['udev:end']
 
-  Anchor['udev:begin'] ->
-  class { 'udev::udevadm::logpriority': udev_log => $udev_log } ->
-  Anchor['udev:end']
+  Anchor['udev:begin']
+  -> class { 'udev::udevadm::logpriority': udev_log => $udev_log }
+  -> Anchor['udev:end']
 
   if $rules {
     create_resources('udev::rule', $rules)
--- manifests/params.pp
+++ manifests/params.pp
@@ -37,7 +37,7 @@ class udev::params {
             $udevtrigger     = 'udevadm trigger --action=change'
             $udevlogpriority = 'udevadm control --log-priority'
           }
-          '7': {
+          '7', '8': {
             $udev_package    = 'systemd'
             $udevtrigger     = 'udevadm trigger --action=change'
             $udevlogpriority = 'udevadm control --log-priority'
